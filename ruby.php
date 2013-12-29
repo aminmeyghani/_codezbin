6a718ef45dbbd1b37446a98c5c1c1fa5e99f032a
Amin Meyghani <amin.meyghani@quovo.com>
2013-12-29 15:07:03 -0500

    more cleanups

--- ruby.php
+++ ruby.php
@@ -1,14 +1,11 @@
 <?php require_once("includes/namespace.php");?>
 <body>
 <?php require_once("topnav.php") ?>
-
 <div id="content" class="maincontent">
 <div id="expand_collapse_container">
 <?php require_once("page_controls.php") ?>
 </div><!--end expand collapse-->
-
 	<ul class="snippet_container">
-
 		<li><a href="#" class="snippet-trigger">Run Ruby - On Mac</a>
 		<div class="snippet">
 			<pre class="brush: ruby;">
@@ -24,14 +21,14 @@
 			#now you are in vim editor. The following
 			#are vi related.
 			[HIT i on keyboard]
-			[TYPE] puts "blah" 
+			[TYPE] puts "blah"
 			[HIT esc  on keyboard]
 			[TYPE] :wq [HIT ENTER] #this will quit and save
 			#now you can run the program:
 			ruby main.rb
 			#outputs:
-			blah</pre>
-
+			blah
+			</pre>
 			</div><!--end snippet-->
 		</li><!--end :) -->
 		<li><a href="#" class="snippet-trigger">Add two numbers - Simple Function</a>
@@ -41,29 +38,19 @@
 			#define functions first and then them.
 			#Add two numbers
 			#
-			
+
 			#Define Function
 			def add(x,y)
 				return x+y
 			end
-			
+
 			#Call function
 			puts add(2,3)
-			
-			
 			</pre>
 			</div><!--end snippet-->
 		</li><!--end :) -->
-		
-		
-		
-		
-
-		
 		</ul><!--end snippet container-->
-	
 	<div id="footer"></div><!--end footer-->
-
 </div><!--end content-->
 </body>
 </html>
